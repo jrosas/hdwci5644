170e6ab0e21e850ff5d07bb4d2afcc3bbee36b1b
Juan V Rosas <jrosas@karasu.(none)>
2012-06-01 08:13:59 -0430

    avance juan

--- bts/user_management/views.py
+++ bts/user_management/views.py
@@ -15,57 +15,47 @@ from forms import FormularioRegistro, FormularioModificarUser,FormularioUsuarioA
 
 
 
-def pre_login(request):
-        dir = "http://127.0.0.1:8000/template/bugtracker/"
-        if request.session.get('uid'):
-                return index(request)
-        return render_to_response("login.html",{'msg': "", 'direccion':dir}, context_instance=RequestContext(request))
-
-
-def logins(request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-                if user.is_active:
-                        login(request,user)
-                        return redirect('iniciada/')
-            # Redirect to a success page.
+def log_in(request):
+        state=''
+        if request.POST:
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                        if user.is_active:
+                                login(request,user)
+                                state='Bienvenido '+user.username
+                        else:
+                                state='Usuario Inactivo'
                 else:
-                        return HttpResponse("Su cuenta esta inactiva.")
-
-            # Return a 'disabled account' error message
-        else:
-                return HttpResponse("Datos invalidos.")
-
-        # Return an 'invalid login' error message.
-
+			state='Datos Invalidos'
+	return render_to_response("login.html" , {'state':state} ,context_instance=RequestContext(request)) 
 
 def logouts(request):
-    logout(request)
+	logout(request)
 
 
 
 def registrarse(request):
-        dir = "http://127.0.0.1:8000/template/bugtracker/"
+	
         if request.method == "GET": # Metodo GET, se va a registrar.
                 if request.session.get('uid'):
                         return inicio(request)
                 f = FormularioRegistro() # Creo un nuevo formulario...
-                return render_to_response("registrarse.html", {'direccion':dir,'msg': "", 'f': f}, # Y se lo paso a la plantilla.
-                                  context_instance=RequestContext(request))
+                return render_to_response("registrarse.html", {'f': f}, # Y se lo paso a la plantilla.
+					  context_instance=RequestContext(request))
 
         elif request.method == "POST": # Se va a registrar
                 f = FormularioRegistro(request.POST)
                 if f.is_valid():
-			try:
+                        try:
                                 u = User.objects.get(username=f.cleaned_data['username']) # Me traigo los datos con f.cleaned_data
                                 return render_to_response("registrarse.html", {'direccion':dir, 'msg': "Nombre de usuario ya existente!",'f' : f},
-                                          context_instance=RequestContext(request))
+							  context_instance=RequestContext(request))
                         except User.DoesNotExist:
                                 if f.cleaned_data['password'] != f.cleaned_data['confirm']:
                                         return render_to_response("registrarse.html",{'direccion':dir,'msg': "Contrase&ntilde;as no coinciden!",'f': f},
-                                              context_instance=RequestContext(request))
+								  context_instance=RequestContext(request))
                                 else:
                                         username=f.cleaned_data['username']
                                         u = User(username=username, email=f.cleaned_data['correo'],password=f.cleaned_data['password'])
@@ -76,11 +66,11 @@ def registrarse(request):
                                         u.is_staff=False
                                         u.is_active=True
                                         u.is_superuser=False
-					u.save()
-                                        return render_to_response("login.html", {'direccion':dir,'msg': "Usuario ya creado!!"},
-                                              context_instance=RequestContext(request))
+                                        u.save()
+                                        return render_to_response("login.html", {'msg' : "Usuario ya creado!!"},
+								  context_instance=RequestContext(request))
                 else:
-                        return render_to_response("registrarse.html",{'direccion':dir, 'msg': "Uno de sus datos no tiene el formato adecuado",'f': FormularioRegistro()})
+                        return render_to_response("registrarse.html",{ 'msg': "Uno de sus datos no tiene el formato adecuado",'f': FormularioRegistro()})
 
 
 
@@ -90,7 +80,7 @@ def modificar_user(request):
         if request.method == "GET":
                 f = FormularioModificarUser()
                 return render_to_response("modificar_user.html", {'direccion':dir,'msg': "", 'f': f},
-                                  context_instance=RequestContext(request))
+					  context_instance=RequestContext(request))
 
         elif request.method == "POST":
                 f = FormularioModificarUser(request.POST)
@@ -115,8 +105,8 @@ def modificar_user(request):
                         return render_to_response("index.html", {'direccion':dir,'msg': "Usuario ya creado!!"},context_instance=RequestContext(request))
 
                 else:
-			 return render_to_response("modificar_user.html", {'direccion':dir,'msg': "Error al modificar usuario", 'f': f},
-                                  context_instance=RequestContext(request))
+			return render_to_response("modificar_user.html", {'direccion':dir,'msg': "Error al modificar usuario", 'f': f},
+						  context_instance=RequestContext(request))
 
 def modificar_admin(request):
         dir = "http://127.0.0.1:8000/template/bugtracker/"
@@ -124,7 +114,7 @@ def modificar_admin(request):
         if request.method == "GET":
                 f =FormularioUsuarioAd()
                 return render_to_response("bugtracker/registrar_error.html", {'direccion':dir,'msg': "", 'f': f},
-                                  context_instance=RequestContext(request))
+					  context_instance=RequestContext(request))
 
         elif request.method == "POST":
                 f = FormularioUsuarioAd(request.POST)
@@ -134,7 +124,7 @@ def modificar_admin(request):
 
                 else:
                         return render_to_response("bugtracker/modificar_admin.html", {'direccion':dir,'msg': "Error al modificar usuario", 'f': f},
-                                  context_instance=RequestContext(request))
+						  context_instance=RequestContext(request))
 
 
 def eliminar_usuario(request,user_iden):
